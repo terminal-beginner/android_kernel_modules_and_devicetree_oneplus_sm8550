32de4a8d7d8a79fdc88cd9b108c070bbb083cace
nullptr-t-oss <127822226+nullptr-t-oss@users.noreply.github.com>
2026-03-17 15:36:28 +0000

    revert: "qcacld-3.0: remove conflicting wlan_hdd_cfg80211_get_channel wrapper"
    This reverts commit 27e7bbcf4389447c02310ae65973f036a4e19790.

--- vendor/qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
+++ vendor/qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
@@ -25916,7 +25916,6 @@ int wlan_hdd_change_hw_mode_for_given_chnl(struct hdd_adapter *adapter,
  * wlan_hdd_cfg80211_get_channel() - Report current operating channel
  * @wiphy: wiphy handle
  * @wdev: wireless_dev handle
- * @link_id: Channel link ID
  * @chandef: output channel definition
  *
  * Required by nl80211 (NL80211_CMD_GET_INTERFACE) and wext (SIOCGIWFREQ)
@@ -25924,62 +25923,6 @@ int wlan_hdd_change_hw_mode_for_given_chnl(struct hdd_adapter *adapter,
  *
  * Return: 0 on success, -ENODATA if no channel is set.
  */
-#ifdef CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT
-static int wlan_hdd_cfg80211_get_channel(struct wiphy *wiphy,
-					 struct wireless_dev *wdev,
-					 unsigned int link_id,
-					 struct cfg80211_chan_def *chandef)
-{
-	struct hdd_adapter *adapter = WLAN_HDD_GET_PRIV_PTR(wdev->netdev);
-	struct hdd_station_ctx *sta_ctx;
-	struct hdd_mon_set_ch_info *ch_info;
-	struct ieee80211_channel *chan;
-	uint32_t freq;
-
-	if (!adapter)
-		return -ENODATA;
-
-	/* Primary source: adapter->mon_chan_freq (set by both boot-time
-	 * monitor mode and cfg80211 set_monitor_channel).
-	 */
-	freq = adapter->mon_chan_freq;
-
-	/* Fallback: station context ch_info (set by hdd_mon_select_cbmode
-	 * in the roam callback path).
-	 */
-	if (!freq) {
-		sta_ctx = WLAN_HDD_GET_STATION_CTX_PTR(adapter);
-		ch_info = &sta_ctx->ch_info;
-		freq = ch_info->freq;
-	}
-
-	if (!freq)
-		return -ENODATA;
-
-	chan = ieee80211_get_channel(wiphy, freq);
-	if (!chan)
-		return -ENODATA;
-
-	cfg80211_chandef_create(chandef, chan, NL80211_CHAN_NO_HT);
-
-	/* Upgrade width if we know the bandwidth */
-	switch (adapter->mon_bandwidth) {
-	case CH_WIDTH_40MHZ:
-		chandef->width = NL80211_CHAN_WIDTH_40;
-		break;
-	case CH_WIDTH_80MHZ:
-		chandef->width = NL80211_CHAN_WIDTH_80;
-		break;
-	case CH_WIDTH_160MHZ:
-		chandef->width = NL80211_CHAN_WIDTH_160;
-		break;
-	default:
-		break;
-	}
-
-	return 0;
-}
-#else
 static int wlan_hdd_cfg80211_get_channel(struct wiphy *wiphy,
 					 struct wireless_dev *wdev,
 					 struct cfg80211_chan_def *chandef)
@@ -26033,7 +25976,6 @@ static int wlan_hdd_cfg80211_get_channel(struct wiphy *wiphy,
 
 	return 0;
 }
-#endif
 
 /**
  * __wlan_hdd_cfg80211_set_mon_ch() - Set monitor mode capture channel
@@ -27349,6 +27291,56 @@ static int __wlan_hdd_cfg80211_get_channel(struct wiphy *wiphy,
 		  chandef->center_freq2);
 	return 0;
 }
+
+/**
+ * wlan_hdd_cfg80211_get_channel() - API to process cfg80211 get_channel request
+ * @wiphy: Pointer to wiphy
+ * @wdev: Pointer to wireless device
+ * @link_id: Channel link ID
+ * @chandef: Pointer to channel definition
+ *
+ * Return: 0 for success, non zero for failure
+ */
+#ifdef CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT
+static int wlan_hdd_cfg80211_get_channel(struct wiphy *wiphy,
+					 struct wireless_dev *wdev,
+					 unsigned int link_id,
+					 struct cfg80211_chan_def *chandef)
+{
+	int errno;
+	struct osif_vdev_sync *vdev_sync;
+
+	errno = osif_vdev_sync_op_start(wdev->netdev, &vdev_sync);
+	if (errno)
+		return errno;
+
+	errno = __wlan_hdd_cfg80211_get_channel(wiphy, wdev, chandef, link_id);
+
+	osif_vdev_sync_op_stop(vdev_sync);
+
+	return errno;
+}
+#else
+static int wlan_hdd_cfg80211_get_channel(struct wiphy *wiphy,
+					 struct wireless_dev *wdev,
+					 struct cfg80211_chan_def *chandef)
+{
+	int errno;
+	struct osif_vdev_sync *vdev_sync;
+	/* Legacy purposes */
+	int link_id = -1;
+
+	errno = osif_vdev_sync_op_start(wdev->netdev, &vdev_sync);
+	if (errno)
+		return errno;
+
+	errno = __wlan_hdd_cfg80211_get_channel(wiphy, wdev, chandef, link_id);
+
+	osif_vdev_sync_op_stop(vdev_sync);
+
+	return errno;
+}
+#endif
 
 static bool hdd_check_bitmask_for_single_rate(enum nl80211_band band,
 				const struct cfg80211_bitrate_mask *mask)
